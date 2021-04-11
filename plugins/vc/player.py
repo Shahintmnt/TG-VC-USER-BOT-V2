3f9e823cb2027c4e850ec8384eb895fd0fb5141d
PHARAOH <70955175+PHARAOH907@users.noreply.github.com>
2021-04-11 18:42:03 +0530

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -160,7 +160,7 @@ async def play_track(client, m: Message):
     playlist = mp.playlist
     # check audio
     if m.audio:
-        if m.audio.duration > 600:
+        if m.audio.duration > 6000:
             reply = await m.reply_text(
                 f"{emoji.ROBOT} audio which duration longer than 10 min "
                 "won't be automatically added to playlist"
